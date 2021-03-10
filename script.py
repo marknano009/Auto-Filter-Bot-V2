95db13df2b8631f9a03ea86d689586b3f3002b87
Mark Nano <78907926+marknano009@users.noreply.github.com>
2021-03-11 00:55:21 +0530

    Update script.py

--- script.py
+++ script.py
@@ -3,64 +3,26 @@ class script(object):
 
     START_MSG = """ <b>Hi {}
 
-You can call this as an Auto Filter Bot if you like :D
+ I'm a Movie Search Bot specially made for Movies Club
 
-This is Version 2 of Auto Filter Bot
+ © @Film_Caffe
+ © @cinema_club_1
+ © @malayalam_movees</b>"""
+ 
 
-Bot gives button link to files in connected channels on query !
-
-No need to add filters for your files or movies from now on!
-
-For more click <i>help</i></b>"""
-
-
-    HELP_MSG = """<b>How to use the bot??</b>
-
-<i>
-* Add bot to your group with admin rights.
-
-* Add bot to channels which you want to link with <b>all admin rights</b>!
-</i>
-
-
-<b>Bot Commands - Works in Group only</b>
-(You need to be a Auth User in order to use these commands)
-
-* <code>/add channelid</code>  -  Links channel to your group.
-or
-* <code>/add @channelusername</code> - Links channel to your group.
-
-<i>NOTE : You can get your channel ID from @ChannelidHEXbot </i>
-
-
-* <code>/del channelid</code>  -  Delinks channel from group
-or
-* <code>/del @channelusername</code>  -  Delinks channel from group
-
-<i>NOTE : You can get connected channel details by <code>/filterstats</code> </i>
-
-
-* <code>/delall</code>  -  Removes all connected channels and filters from group!
-
-* <code>/filterstats</code>  -  Check connected channels and number of filters.
-
-
-
-No need add each filter again!
-Bot will automatically search for your files and give links to that!
-
-
-<b>© @TroJanzHEX</b>"""
-
-
-    ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot V2</b>
+    HELP_MSG = """<b>No one Gonna help you 🤣😂🤣
     
-⭕️<b>Creater :</b> @TroJanzHEX
+    © @Film_Caffe
+    © @cinema_club_1
+    © @Malayalam_Movees</b>"""
+
+
+    ABOUT_MSG = """⭕️<b>My Name : Movies Club Auto Filter Bot</b>
+    
+⭕️<b>Creater :</b> @Malayalam_Movees
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
-⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
-
-⭕️<b>Tutorial Video :</b> <a href='https://youtu.be/KQVYQAOsFYY'>Video Link</a> 
+⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a>  
 
 """
